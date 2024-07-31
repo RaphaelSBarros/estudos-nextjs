af9189fe5cb1af1610bc560eedf106d4165ca456
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-07-31 14:06:09 -0300

    Rotas Dinâmicas com vários parâmetros

---
+++ src/app/shop/[...slug]/page.tsx
@@ -0,0 +1,14 @@
+type Props = {
+    params: {
+        slug: string[]
+    }
+}
+
+const Page = ({ params }: Props) => {
+    return (
+        <div>
+            Filtros: {params.slug.join(", ")}
+        </div>
+    )
+}
+export default Page;
