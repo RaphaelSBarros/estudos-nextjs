fde987b7dfee123ddd50db0b860316ba51cbe302
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-08-01 11:02:06 -0300

    Rotas dinamicamente estáticas

--- src/app/blog/[postId]/page.tsx
+++ src/app/blog/[postId]/page.tsx
@@ -1,5 +1,4 @@
 import { Post } from "@/types/Post"
-import { Metadata } from "next"
 
 type Props = {
   params: {
@@ -7,26 +6,28 @@ type Props = {
   }
 }
 
-export const generateMetadata = async ({ params }: Props) => {
-  const postRequest = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
-  const post: Post = await postRequest.json()
-
-  return {
-    title: post.title
-  }
-}
-
 const Page = async ({ params }: Props) => {
   const postRequest = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
   const post: Post = await postRequest.json()
+
+  if (!post.id) return <div>Post não encontrado!</div>
 
   return(
     <div className="text-center">
       <div>ID: {post.id}</div>
       <h1 className="text-3xl uppercase my-3">{post.title}</h1>
       <p>{post.body}</p>
-  </div>
+    </div>
   )
 }
 
 export default Page;
+
+export const generateStaticParams = async () => {
+  const postsReq = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts: Post[] = await postsReq.json();
+
+  return posts.map(post => ({
+    postId: post.id.toString()
+  }));
+}
